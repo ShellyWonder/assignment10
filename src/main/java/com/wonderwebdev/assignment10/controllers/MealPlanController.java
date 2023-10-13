dac0e4fcddfdc7d51decc18530f2c4538cee1c21
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2023-10-13 17:47:24 -0500

    made changes to the MealPlanController.

--- src/main/java/com/wonderwebdev/assignment10/controllers/MealPlanController.java
+++ src/main/java/com/wonderwebdev/assignment10/controllers/MealPlanController.java
@@ -1,7 +1,9 @@
 package com.wonderwebdev.assignment10.controllers;
 
-import com.wonderwebdev.assignment10.dto.DayResponse;
-import com.wonderwebdev.assignment10.dto.WeekResponse;
+import com.wonderwebdev.assignment10.dto.Response;
+import com.wonderwebdev.assignment10.services.MealPlanService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,20 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MealPlanController {
 
+    @Autowired
+    private MealPlanService mealPlanService;
+
     @GetMapping("/mealplanner/week")
-    public ResponseEntity<WeekResponse> getWeekMeals(String numCalories, String diet, String exclusions){
-        WeekResponse response = new WeekResponse();
-        response.setNumCalories(numCalories);
-        response.setDiet(diet);
-        response.setExclusions(exclusions);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public ResponseEntity<Response> getWeeklyMeals(String targetCalories, String diet, String exclude) {
+        Response response = mealPlanService.getWeeklyMeals(targetCalories, diet, exclude);
+        return new ResponseEntity<Response>(response, HttpStatus.OK);
     }
     @GetMapping("/mealplanner/day")
-    public ResponseEntity<DayResponse> getDayMeals(String numCalories, String diet, String exclusions){
-        DayResponse response = new DayResponse();
-        response.setNumCalories(numCalories);
-        response.setDiet(diet);
-        response.setExclusions(exclusions);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public ResponseEntity<Response> getDailyMeals(String targetCalories, String diet, String exclude) {
+        Response response = mealPlanService.getDailyMeals(targetCalories, diet, exclude);
+        return new ResponseEntity<Response>(response, HttpStatus.OK);
     }
 }
